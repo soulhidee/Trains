169361691d9dd33bac9f0276ee4a5af75186b648
soulhidee <serverpgig@yandex.ru>
2025-10-16 15:02:20 +0300

    first commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Даниил on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
