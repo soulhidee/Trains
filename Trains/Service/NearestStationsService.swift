4aa7c5894c8a202eae8cc86baed7001612e48ff4
soulhidee <serverpgig@yandex.ru>
2025-10-19 14:15:58 +0300

    ref(NearestStationsService.swift): add Marks

--- Trains/Service/NearestStationsService.swift
+++ Trains/Service/NearestStationsService.swift
@@ -1,22 +1,28 @@
 import OpenAPIRuntime
 import OpenAPIURLSession
 
+// MARK: - Typealiases
 typealias NearestStations = Components.Schemas.Stations
 
+// MARK: - Protocol
 protocol NearestStationsServiceProtocol {
     func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
 }
 
-
+// MARK: - Service Implementation
 final class NearestStationsService: NearestStationsServiceProtocol {
+    
+    // MARK: - Private Properties
     private let client: Client
     private let apiKey: String
     
+    // MARK: - Initialization
     init(client: Client, apiKey: String) {
         self.client = client
         self.apiKey = apiKey
     }
     
+    // MARK: - Public Methods
     func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations {
         let response = try await client.getNearestStations(query: .init(
             apikey: apiKey,
