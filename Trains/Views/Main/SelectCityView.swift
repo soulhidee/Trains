64900d413e65e6740e5a82347dd2b8ddfc9acd59
soulhidee <serverpgig@yandex.ru>
2025-11-12 14:37:08 +0300

    ref(SelectCityView)

--- Trains/Views/Main/SelectCityView.swift
+++ Trains/Views/Main/SelectCityView.swift
@@ -4,40 +4,23 @@ import SwiftUI
 
 struct SelectCityView: View {
     let onSelect: (String) -> Void
-    @State private var searchText = ""
-
-    
     private let cities = [
-        City(name: "Москва"),
-        City(name: "Санкт Петербург"),
-        City(name: "Сочи"),
-        City(name: "Горный воздух"),
-        City(name: "Краснодар"),
-        City(name: "Казань"),
-        City(name: "Омск"),
+        "Москва",
+        "Санкт Петербург",
+        "Сочи",
+        "Горный воздух",
+        "Краснодар",
+        "Казань",
+        "Омск",
     ]
     
-    
     var body: some View {
-//        NavigationStack {
-//            List(filterCities) { city in
-//                RowCityView(city: city)
-//                    .listRowSeparator(.hidden)
-//            }
-//            
-//            .navigationTitle("Выбор города")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .searchable(text: $searchText, prompt: "Введите запрос")
-//            .overlay {
-//                if filterCities.isEmpty && !searchText.isEmpty {
-//                    Text("Город не найден")
-//                        .font(.system(size: 24, weight: .bold))
-//                }
-//            }
-//            
-//        }
-//        .environment(\.defaultMinListRowHeight, 60)
-//        .listStyle(.plain)
+        SelectionListView(
+            title: "Выбор города",
+            searchPrompt: "Введите запрос",
+            emptyMassage: "Город не найден",
+            items: cities,
+            onSelect: onSelect)
         
     }
 }
