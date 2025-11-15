48ff44c83710c96820f399f1c6f947d49b0a8e6d
soulhidee <serverpgig@yandex.ru>
2025-11-15 17:34:02 +0300

    feat: Filter model

---
+++ Trains/Views/Main/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  Trains
+//
+//  Created by Даниил on 15.11.2025.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterView()
+}
