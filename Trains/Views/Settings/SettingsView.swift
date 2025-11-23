ec47c3115acd62549a2e4a75a094034d356ff308
soulhidee <serverpgig@yandex.ru>
2025-11-23 14:08:26 +0300

    feat: add SettingsRowView

--- Trains/Views/Settings/SettingsView.swift
+++ Trains/Views/Settings/SettingsView.swift
@@ -3,8 +3,7 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         VStack {
-            ErrorView(image: Image(.errorServer), title: "Ошибка сервера")
-                .background(Color.ypWhite)
+
         }
         
         .frame(maxWidth: .infinity, maxHeight: .infinity)
