8ff6100bbee8fba6c7517350944e6e11972e029a
soulhidee <serverpgig@yandex.ru>
2025-11-27 17:04:33 +0300

    feat: StoryView

--- Trains/Model/Story.swift
+++ Trains/Model/Story.swift
@@ -10,19 +10,19 @@ struct Story {
     static let story1 = Story(
         backgroundImage: Image(.story1),
         title: "Text Text Text Text Text Text Text Text Text Text",
-        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
     )
     
     static let story2 = Story(
         backgroundImage: Image(.story2),
         title: "Text Text Text Text Text Text Text Text Text Text",
-        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
     )
     
     static let story3 = Story(
         backgroundImage: Image(.story3),
         title: "Text Text Text Text Text Text Text Text Text Text",
-        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
     )
 
 
