ce3edf0d8638c30d383ba9731531723deeaf0268
soulhidee <serverpgig@yandex.ru>
2025-11-27 17:10:07 +0300

    feat: StoryContentView

---
+++ Trains/Views/Main/Story/StoryContentView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryContentView.swift
+//  Trains
+//
+//  Created by Даниил on 27.11.2025.
+//
+
+import SwiftUI
+
+struct StoryContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryContentView()
+}
