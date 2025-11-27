328fb14dbaa6062652d0e4b953974ee3035d55ce
soulhidee <serverpgig@yandex.ru>
2025-11-27 15:49:35 +0300

    feat: CarrierCardView

--- Trains/Views/Main/Carrier/CarrierCardView.swift
+++ Trains/Views/Main/Carrier/CarrierCardView.swift
@@ -1,39 +1,65 @@
-//
-//  CarrierCardView.swift
-//  Trains
-//
-//  Created by Даниил on 16.11.2025.
-//
-
 import SwiftUI
 
 struct CarrierCardView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            Text("Тут будет информация о перевозчике")
-                .navigationTitle("Информация о перевозчике")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarBackButtonHidden(true)
-            
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Image(systemName: "chevron.left")
-                                .font(.system(size: 17, weight: .semibold))
-                                .foregroundStyle(.ypBlack)
-                        }
+        ScrollView {
+            VStack(spacing: 0) {
+                Image(.rjdLogo)
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: 104)
+                    .padding(.top, 16)
+
+                VStack(alignment: .leading, spacing: 24) {
+                    Text("ОАО «РЖД»")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.ypBlack)
+
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("E-mail")
+                            .font(.system(size: 17))
+                            .foregroundColor(.ypBlack)
+                        
+                        Link("i.lozgkina@yandex.ru", destination: URL(string: "mailto:i.lozgkina@yandex.ru")!)
+                            .font(.system(size: 12))
+                            .foregroundColor(.ypBlue)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Телефон")
+                            .font(.system(size: 17))
+                            .foregroundColor(.ypBlack)
+                        
+                        Link("+7 (904) 329-27-71", destination: URL(string: "tel:+79043292771")!)
+                            .font(.system(size: 12))
+                            .foregroundColor(.ypBlue)
                     }
                 }
-            
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 16)
+                .padding(.top, 32)
+                
+                Spacer()
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .navigationTitle("Информация о перевозчике")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .font(.system(size: 17, weight: .semibold))
+                        .foregroundStyle(.ypBlack)
+                }
+            }
+        }
         .background(Color.ypWhite)
     }
-    
 }
 
 #Preview {
