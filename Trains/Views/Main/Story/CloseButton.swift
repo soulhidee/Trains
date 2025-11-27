d2cbca33a647285b780316432889dfa97d814e1c
soulhidee <serverpgig@yandex.ru>
2025-11-27 16:44:20 +0300

    feat: closeButton

---
+++ Trains/Views/Main/Story/CloseButton.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct CloseButton: View {
+    let action: () -> Void
+
+    var body: some View {
+        Button("", image: .close) {
+            action()
+        }
+    }
+}
+
+#Preview {
+    CloseButton {
+        print("Жмяк")
+    }
+}
