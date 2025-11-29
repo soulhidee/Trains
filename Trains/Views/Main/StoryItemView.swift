4553f5c19c816688cc6ce34bc70a951c6049429b
soulhidee <serverpgig@yandex.ru>
2025-11-29 13:35:01 +0300

    fix(StoryItemView): opacity and text weight

--- Trains/Views/Main/StoryItemView.swift
+++ Trains/Views/Main/StoryItemView.swift
@@ -11,17 +11,18 @@ struct StoryItemView: View {
                 .scaledToFill()
                 .frame(width: 92, height: 140)
                 .clipShape(RoundedRectangle(cornerRadius: 16))
-                .opacity(isViewed ? 0.4 : 1.0)
+                .opacity(isViewed ? 0.5 : 1.0)
             
             VStack {
                 Spacer()
                 Text(story.title)
-                    .font(.system(size: 12, weight: .medium))
-                    .foregroundColor(.white)
+                    .font(.system(size: 12, weight: .regular))
+                    .foregroundColor(.ypWhiteUniversal)
                     .lineLimit(3)
                     .multilineTextAlignment(.leading)
                     .padding(.horizontal, 8)
                     .padding(.bottom, 12)
+                    .opacity(isViewed ? 0.5 : 1.0)
             }
             .frame(width: 92, height: 140)
         }
@@ -35,5 +36,5 @@ struct StoryItemView: View {
 }
 
 #Preview {
-    StoryItemView(story: Story.story1, isViewed: false)
+    StoryItemView(story: Story.story4, isViewed: true)
 }
