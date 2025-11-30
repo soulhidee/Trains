f789ed9770c94ff0ba9bbcc5f8776cb3109316a4
soulhidee <serverpgig@yandex.ru>
2025-11-30 13:25:54 +0300

    fix

--- Trains/Managers/ThemeManager.swift
+++ Trains/Managers/ThemeManager.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 import Combine
 
-class ThemeManager: ObservableObject {
+final class ThemeManager: ObservableObject {
     @Published var isDarkMode: Bool {
         didSet {
             UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
         }
     }
     
+    var colorScheme: ColorScheme {
+        isDarkMode ? .dark : .light
+    }
+    
     init() {
         self.isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
     }
     
-    var colorScheme: ColorScheme {
-        isDarkMode ? .dark : .light
-    }
+
 }
